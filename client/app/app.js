9cb246bddd50c0f768a6f260da65ca5d3a6b2ae6
xfredlix <fred.m.li.93@gmail.com>
2017-04-19 14:28:22 -0700

    added comments

--- client/app/app.js
+++ client/app/app.js
@@ -3,11 +3,21 @@ angular.module('app', [
   ])
 
 .controller('notePadController', function ($scope, notePad) {
+  //controllers take in parameters, just as our model
+  //the scope is whatever the index.html page has access to
+  //teh index page knows to use this controller because we put ng-controller = to this one
   $scope.notes = [];
+  //we can invoke any property from scope in the index.html function. thats where
+  //i used this save notes function
+  // see below for more examples
   $scope.saveNotes = notes => {
     $scope.notes.push(notes);
     console.log(notes)
   } 
+});
+
+
+
   // page = $scope.page;
   // saver.loadPages().then( function(response) {
   //   page.links = response.data;
@@ -37,4 +47,3 @@ angular.module('app', [
   //     "\n" + document.body.getElementsByClassName('text')[1].text, 15, 15);
   //   doc.save('page.pdf')
   // }
-});
--- client/app/notePad.js
+++ client/app/notePad.js
@@ -1,6 +1,20 @@
 angular.module('app.service', [])
 
 .factory('notePad', function($http) {
+  //factories are the models, it can write functions and 
+  //return an object holding the functions
+  //see below for an example. Anything we need can be passed into the parameter
+  //for us, we put in the http as a parameter, check documentation for more examples
+  return {};
+});
+
+
+
+
+
+
+
+
   // var savePage = function(page) {
   //   return $http({
   //     method: 'POST',
@@ -29,5 +43,3 @@ angular.module('app.service', [])
   //   savePage: savePage,
   //   loadPages: loadPages
   // }
-  return {};
-});
