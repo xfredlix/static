7e0b50cfa0adf0671dc123c9bcb061229790d636
DanielLChang <daniellawrencechang@gmail.com>
2017-05-11 20:05:06 -0700

    App component

---
+++ src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="app">
+    <h1>Static</h1>
+  </div>
+);
+
+export default App;
--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-// import App from './app';
+import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    // <App />
+    <App />
   </Provider>
 );
+
+export default Root;
