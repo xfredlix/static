f84fac827c3e27fb756f5c478affac593db1a11f
DanielLChang <daniellawrencechang@gmail.com>
2017-05-11 20:03:32 -0700

    Root component

---
+++ src/components/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+// import App from './app';
+
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    // <App />
+  </Provider>
+);
