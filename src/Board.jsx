7441656b46e6a549367c230a6f9eef25973b6039
DanielLChang <daniellawrencechang@gmail.com>
2017-05-12 17:31:41 -0700

    Create board class

---
+++ src/Board.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <p>hello</p>
+      </div>
+    );
+  }
+}
+
+export default Board;
