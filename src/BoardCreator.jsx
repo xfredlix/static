da2c9c7b27928f995b4a083f130c1530fa3c727c
DanielLChang <daniellawrencechang@gmail.com>
2017-05-13 20:23:39 -0700

    Create BoardCreator class

---
+++ src/BoardCreator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class BoardCreator extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
