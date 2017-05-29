b7ca7b04aeae595278326606d25d060533bb8dd1
xfredlix <fred.m.li.93@gmail.com>
2017-05-29 14:34:47 -0700

    updated list thread

--- src/ListThread.jsx
+++ src/ListThread.jsx
@@ -19,12 +19,34 @@ export default class ListThread extends React.Component {
     this.setState(newState);
   }
 
+  addToList(index) {
+    const newState = Object.assign({}, this.state);
+    newState.lists[index].push(this.state.cardToBeChanged);
+    this.setState(newState);
+  }
+
+  changeCard(card) {
+    this.setState({
+      changeList: true
+    })
+  }
+
   renderLists() {
     return (
       this.state.lists.map((list, idx) => {
-        return <List key={idx} listTitle={list} />;
+        return (<div> 
+          <List key={idx} listTitle={list} />
+          {this.renderSelectList(idx)}
+          </div>
+        )
       })
     );
+  }
+
+  renderSelectList(index) {
+    if (this.state.changeList && this.state.cardToBeChanged) return (
+      <button onClick={this.addToList.bind(this, index)}> add card to this list </button>
+      )
   }
 
   render() {
